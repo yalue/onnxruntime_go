f9cafdda92c2e5e095a313fa57b4cabed1c4e607
Rob Keevil <robkeevil@gmail.com>
2024-11-11 13:45:07 +0100

    upgrade to onnxruntime 1.20.0

--- onnxruntime_go.go
+++ onnxruntime_go.go
@@ -418,9 +418,9 @@ const (
 	TensorElementDataTypeUint32    = C.ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT32
 	TensorElementDataTypeUint64    = C.ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT64
 
-	// Not supported by onnxruntime (as of onnxruntime version 1.19.0)
+	// Not supported by onnxruntime (as of onnxruntime version 1.20.0)
 	TensorElementDataTypeComplex64 = C.ONNX_TENSOR_ELEMENT_DATA_TYPE_COMPLEX64
-	// Not supported by onnxruntime (as of onnxruntime version 1.19.0)
+	// Not supported by onnxruntime (as of onnxruntime version 1.20.0)
 	TensorElementDataTypeComplex128 = C.ONNX_TENSOR_ELEMENT_DATA_TYPE_COMPLEX128
 
 	// Non-IEEE floating-point format based on IEEE754 single-precision
